23a9e07f2e213898ed9c4a20d32e117b7c9cafa2
krasotun <servrostov@gmail.com>
2023-03-29 17:12:22 +0300

    add: card component

--- src/app/pages/products-list/products-list.module.ts
+++ src/app/pages/products-list/products-list.module.ts
@@ -1,9 +1,10 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {ProductsListComponent} from './products-list.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
-	declarations: [ProductsListComponent],
+	declarations: [ProductsListComponent, CardComponent],
 	imports: [CommonModule],
 	exports: [ProductsListComponent],
 })
