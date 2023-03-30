55edf332f0eb57d4a4d3e26172164888060fb065
krasotun <servrostov@gmail.com>
2023-03-30 16:00:16 +0300

    feat: card page

--- src/app/pages/products-list/card/card.component.ts
+++ src/app/pages/products-list/card/card.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+import {IProduct} from 'src/app/shared/products/product.interface';
+import {productMock} from 'src/app/shared/products/product.mock';
 
 @Component({
-  selector: 'app-card',
-  templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+	selector: 'app-card',
+	templateUrl: './card.component.html',
+	styleUrls: ['./card.component.css'],
 })
-export class CardComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class CardComponent {
+	readonly item: IProduct = productMock;
 }
--- src/app/pages/products-list/products-list.module.ts
+++ src/app/pages/products-list/products-list.module.ts
@@ -1,11 +1,13 @@
 import {NgModule} from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
 import {CommonModule} from '@angular/common';
 import {ProductsListComponent} from './products-list.component';
-import { CardComponent } from './card/card.component';
+import {CardComponent} from './card/card.component';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
 	declarations: [ProductsListComponent, CardComponent],
-	imports: [CommonModule],
+	imports: [CommonModule, MatCardModule, MatButtonModule],
 	exports: [ProductsListComponent],
 })
 export class ProductsListModule {}
