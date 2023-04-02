c739bdf9726babba0a587c33d20c810ebdf33ec9
tsyk007 <tsykunov.vb@gmail.com>
2023-04-02 16:59:08 +0300

    feat: add card module

--- src/app/pages/products-list/products-list.module.ts
+++ src/app/pages/products-list/products-list.module.ts
@@ -1,13 +1,11 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {ProductsListComponent} from './products-list.component';
-import {CardComponent} from './card/card.component';
-import {MatCardModule} from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
+import {CardModule} from './card/card.module';
 
 @NgModule({
-	declarations: [ProductsListComponent, CardComponent],
-	imports: [CommonModule, MatCardModule, MatButtonModule],
+	declarations: [ProductsListComponent],
+	imports: [CommonModule, CardModule],
 	exports: [ProductsListComponent],
 })
 export class ProductsListModule {}
