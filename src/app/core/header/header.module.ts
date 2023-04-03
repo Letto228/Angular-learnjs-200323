34956b2692eb3c20715e5ece339c4985f014e9e0
Amalaniy.IS <iamalaniy@solit-clouds.ru>
2023-04-03 10:55:59 +0300

    feat(дз): lecture-2 Добавил домашку

--- src/app/core/header/header.module.ts
+++ src/app/core/header/header.module.ts
@@ -1,12 +1,12 @@
 import {NgModule} from '@angular/core';
 import {HeaderComponent} from './header.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
 	declarations: [HeaderComponent],
-	imports: [MatToolbarModule, MatIconModule, MatButtonModule],
+	imports: [MatIconModule, MatToolbarModule, MatButtonModule],
 	exports: [HeaderComponent],
 })
 export class HeaderModule {}
--- src/app/pages/products-list/products-list.component.ts
+++ src/app/pages/products-list/products-list.component.ts
@@ -5,4 +5,24 @@ import {Component} from '@angular/core';
 	templateUrl: './products-list.component.html',
 	styleUrls: ['./products-list.component.css'],
 })
-export class ProductsListComponent {}
+export class ProductsListComponent {
+	size = 4;
+	image1 =
+		'https://cdn.vietnammoi.vn/2019/10/3/gia-iphone-11-xach-tay-bat-ngo-ve-duoi-20-trieu-dong-15701184406251659163712.jpg';
+	image2 =
+		'https://www.hdretail.ru/upload/iblock/8d0/8d01c02a7a68c45e7d021d9d44efab9f.jpg';
+	image3 =
+		'http://ranbus.fra1.cdn.digitaloceanspaces.com/ranbus/media/images/2021/08/26/1629999515_HFxP8b1KqR.jpg';
+	image4 = 'https://appleinsider.ru/wp-content/uploads/2022/05/14pro_renders.jpeg';
+
+	items = [
+		{id: 1, name: 'iphone 11', price: '12 000р', image: this.image1},
+		{id: 2, name: 'iphone 12', price: '18 000р', image: this.image2},
+		{id: 3, name: 'iphone 13', price: '36 000р', image: this.image3},
+		{id: 4, name: 'iphone 14', price: '68 000р', image: this.image4},
+	];
+
+	clickDisable() {
+		console.log('click card');
+	}
+}
