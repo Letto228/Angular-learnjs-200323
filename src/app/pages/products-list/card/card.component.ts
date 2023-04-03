0fd498438fa0c11cb4c98296a216770ae45162fb
Василий Кузнецов <basic-1@mail.ru>
2023-04-03 17:48:13 +0300

    feat: lecture 2 dz

--- src/app/pages/products-list/card/card.component.ts
+++ src/app/pages/products-list/card/card.component.ts
@@ -5,4 +5,8 @@ import {Component} from '@angular/core';
 	templateUrl: './card.component.html',
 	styleUrls: ['./card.component.css'],
 })
-export class CardComponent {}
+export class CardComponent {
+	onClick() {
+		alert('собакен куплен.');
+	}
+}
