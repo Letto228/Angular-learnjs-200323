f0d0a6445683f58bbc91176c54d35472d393c899
Letto228 <learnjs@MacBook-Pro-Egor.local>
2023-04-06 19:41:54 +0300

    chore: pre lecture 5

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,18 +1,4 @@
-import {
-	AfterContentChecked,
-	AfterContentInit,
-	AfterViewChecked,
-	AfterViewInit,
-	Component,
-	DoCheck,
-	EventEmitter,
-	Input,
-	OnChanges,
-	OnDestroy,
-	OnInit,
-	Output,
-	SimpleChanges,
-} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {IApplicationConfig} from '../../shared/application-config/application-config.interface';
 
 @Component({
@@ -20,54 +6,8 @@ import {IApplicationConfig} from '../../shared/application-config/application-co
 	templateUrl: './header.component.html',
 	styleUrls: ['./header.component.css'],
 })
-// export class HeaderComponent implements OnChanges, OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
 export class HeaderComponent {
 	@Input() applicationConfig: IApplicationConfig | undefined;
-	@Input() applicationConfig1: IApplicationConfig | undefined;
-	@Input() applicationConfig2: IApplicationConfig | undefined;
-	@Input() applicationConfig3: IApplicationConfig | undefined;
-	@Input() applicationConfig4: IApplicationConfig | undefined;
 
 	@Output() menuClick = new EventEmitter<void>();
-
-	// ngOnChanges({applicationConfig}: SimpleChanges) {
-	// ngOnChanges(changes: SimpleChanges) {
-	// const applicationConfig = changes['applicationConfig'];
-
-	// console.log(changes);
-
-	// if (applicationConfig && this.applicationConfig?.title !== applicationConfig.previousValue?.title) {
-	// 	// input applicationConfig is changed
-	// 	// this.applicationConfig === applicationConfig.currentValue
-	// 	console.log('is title changed');
-	// }
-	// }
-
-	// ngOnInit() {
-
-	// }
-
-	// ngDoCheck() {
-
-	// }
-
-	// ngAfterContentInit(): void {
-
-	// }
-
-	// ngAfterContentChecked(): void {
-
-	// }
-
-	// ngAfterViewInit(): void {
-
-	// }
-
-	// ngAfterViewChecked(): void {
-
-	// }
-
-	// ngOnDestroy(): void {
-
-	// }
 }
--- src/app/core/sidenav/sidenav.component.ts
+++ src/app/core/sidenav/sidenav.component.ts
@@ -1,21 +1,4 @@
-import {
-	AfterViewInit,
-	Component,
-	ContentChild,
-	ElementRef,
-	Input,
-	OnChanges,
-	OnInit,
-	QueryList,
-	SimpleChanges,
-	TemplateRef,
-	ViewChild,
-	ViewChildren,
-	ViewContainerRef,
-	ViewRef,
-} from '@angular/core';
-import {MatButton} from '@angular/material/button';
-import {MatList} from '@angular/material/list';
+import {Component, ViewChild} from '@angular/core';
 import {MatDrawer} from '@angular/material/sidenav';
 
 @Component({
@@ -23,59 +6,11 @@ import {MatDrawer} from '@angular/material/sidenav';
 	templateUrl: './sidenav.component.html',
 	styleUrls: ['./sidenav.component.css'],
 })
-// export class SidenavComponent implements OnChanges, OnInit, AfterViewInit {
 export class SidenavComponent {
-	// @Input() navigationTemplate: TemplateRef<any> | undefined;
-
 	@ViewChild(MatDrawer)
 	private readonly drawerComponent!: MatDrawer;
 
-	@ViewChild(MatButton)
-	private readonly matButton!: MatButton;
-	@ViewChild(MatButton, {read: ElementRef, static: true})
-	private readonly matButtonElementRef: ElementRef | undefined;
-
-	@ViewChild('viewport', {read: ViewContainerRef, static: true})
-	private readonly viewport!: ViewContainerRef;
-
-	// @ViewChildren(MatButton)
-	// private readonly matButtons!: QueryList<MatButton>;
-
-	@ContentChild(MatList, {static: true, read: ElementRef})
-	private readonly matList!: ElementRef;
-
-	// ngOnChanges({navigationTemplate}: SimpleChanges): void {
-	// 	if (navigationTemplate) {
-	// 		this.insertNavigationTemplate();
-	// 	}
-	// }
-
 	toggleSidenavOpened() {
-		// console.log(this.matButtons);
-		// console.log(this.matButtonElementRef);
-		console.log(this.matList);
 		this.drawerComponent.toggle();
 	}
-
-	// ngOnInit(): void {
-	// console.log('ngOnInit', this.matButtonElementRef);
-	// this.insertNavigationTemplate();
-	// }
-
-	// ngAfterViewInit(): void {
-	// console.log('ngAfterViewInit', this.matButtonElementRef);
-	// }
-
-	// private insertNavigationTemplate() {
-	// if (this.navigationTemplate) {
-	// this.viewport.createEmbeddedView(this.navigationTemplate);
-	// this.viewport.createEmbeddedView(this.navigationTemplate, {firstName: 'is 2'});
-	// this.viewport.createEmbeddedView(this.navigationTemplate, {firstName: 'is 3'});
-
-	// this.viewport.move(
-	// 	this.viewport.get(2) as ViewRef,
-	// 	0
-	// );
-	// }
-	// }
 }
