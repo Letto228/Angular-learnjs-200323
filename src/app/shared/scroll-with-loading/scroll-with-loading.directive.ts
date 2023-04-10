94902142eb7925bb8f277fc09fc11861ec6717af
tsyk007 <tsykunov.vb@gmail.com>
2023-04-10 13:21:40 +0300

    feat: lecture-5 dz

--- src/app/shared/scroll-with-loading/scroll-with-loading.directive.ts
+++ src/app/shared/scroll-with-loading/scroll-with-loading.directive.ts
@@ -10,27 +10,27 @@ export class ScrollWithLoadingDirective {
 	@Output() loadData = new EventEmitter<LoadDirection>();
 
 	lastScrollTop = 0;
-	lastClientHeightOnAfter = 0;
-	lastClientHeightOnBefore = 0;
+	lastScrollHeightOnAfter = 0;
+	lastScrollHeightOnBefore = 0;
 	@HostListener('scroll', ['$event.target'])
 	onScroll(element: HTMLElement) {
 		const scrollTop = element.scrollTop;
 		if (
 			scrollTop > this.lastScrollTop &&
 			scrollTop + element.clientHeight >= element.scrollHeight - offset &&
-			element.clientHeight !== this.lastClientHeightOnAfter
+			element.scrollHeight !== this.lastScrollHeightOnAfter
 		) {
 			this.loadData.emit(LoadDirection.after);
-			this.lastClientHeightOnAfter = element.clientHeight;
+			this.lastScrollHeightOnAfter = element.scrollHeight;
 		}
 
 		if (
 			scrollTop < this.lastScrollTop &&
 			scrollTop <= offset &&
-			element.clientHeight !== this.lastClientHeightOnBefore
+			element.scrollHeight !== this.lastScrollHeightOnBefore
 		) {
 			this.loadData.emit(LoadDirection.before);
-			this.lastClientHeightOnBefore = element.clientHeight;
+			this.lastScrollHeightOnBefore = element.scrollHeight;
 		}
 		this.lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
 	}
