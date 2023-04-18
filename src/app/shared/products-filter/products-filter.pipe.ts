d3aa22678550c3b639f344de1f6b1f01d34f2c3b
Bender <mu57di3@gmail.com>
2023-04-18 14:34:36 +0300

    feat: добавил возможность передачи не только строковых значений в качестве фильтра

--- src/app/shared/products-filter/products-filter.pipe.ts
+++ src/app/shared/products-filter/products-filter.pipe.ts
@@ -4,15 +4,15 @@ import {Pipe, PipeTransform} from '@angular/core';
 	name: 'filterByProperty',
 })
 export class ProductsFilterPipe implements PipeTransform {
-	transform<T>(value: Array<T>, property: keyof T, search: string): Array<T> {
+	transform<T>(value: Array<T>, property: keyof T, search: string | number | boolean | null): Array<T> {
 		return value.filter((item: T) => {
 			switch (typeof item[property]) {
 				case 'string':
 					return String(item[property]).startsWith(search as string);
 				case 'number':
-					return item[property] === parseFloat(search);
+					return item[property] === parseFloat(search as string);
 				case 'boolean':
-					return item[property] === (search === 'false' || true);
+					return item[property] === (search === 'false' || !!search);
 				default:
 					return item[property] === search;
 			}
