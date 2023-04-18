ecf294f20cab22c081073edecdce701581498b84
Bender <mu57di3@gmail.com>
2023-04-18 14:27:12 +0300

    feat: добавил обработку типа boolean

--- src/app/shared/products-filter/products-filter.pipe.ts
+++ src/app/shared/products-filter/products-filter.pipe.ts
@@ -11,6 +11,8 @@ export class ProductsFilterPipe implements PipeTransform {
 					return String(item[property]).startsWith(search);
 				case 'number':
 					return item[property] === parseFloat(search);
+        case 'boolean':
+          return item[property] === (search === 'true' || false)
 				default:
 					return item[property] == search;
 			}
