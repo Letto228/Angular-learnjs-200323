60168a4c029042520b2e68c415ed321f8db92658
Василий Кузнецов <basic-1@mail.ru>
2023-04-20 06:01:58 +0300

    feat: dz6

--- src/app/shared/pagination/pagination.directive.ts
+++ src/app/shared/pagination/pagination.directive.ts
@@ -20,7 +20,7 @@ export class PaginationDirective<T> implements OnInit, OnChanges, OnDestroy {
 	// Количество элементов в чанке
 	@Input() appPaginationChankSize = 1;
 
-	private chank: Array<T[]> = [];
+	private chanks: Array<T[]> = [];
 
 	private readonly currentIndex$ = new BehaviorSubject<number>(0);
 	private readonly destroy$ = new Subject<void>();
@@ -38,7 +38,7 @@ export class PaginationDirective<T> implements OnInit, OnChanges, OnDestroy {
 				return;
 			}
 
-			this.chank = getChank(this.appPaginationOf, this.appPaginationChankSize);
+			this.chanks = getChank(this.appPaginationOf, this.appPaginationChankSize);
 			this.currentIndex$.next(0);
 		}
 	}
@@ -59,7 +59,7 @@ export class PaginationDirective<T> implements OnInit, OnChanges, OnDestroy {
 				map(index =>
 					this.getCurrentContext(
 						index,
-						this.chank,
+						this.chanks,
 						this.appPaginationChankSize,
 					),
 				),
@@ -92,17 +92,32 @@ export class PaginationDirective<T> implements OnInit, OnChanges, OnDestroy {
 		};
 	}
 
+	// private next2() {
+	// 	console.log(this.currentIndex$);
+	// 	const nextIndex = this.currentIndex$.value + 1;
+	// 	const maxIndex =
+	// 		Math.round(
+	// 			(this.appPaginationOf as T[]).length / this.appPaginationChankSize,
+	// 		) - 1;
+	// 	const newIndex = nextIndex <= maxIndex ? nextIndex : maxIndex;
+	// 	if (this.currentIndex$.value == maxIndex) {
+	// 		console.log('Последняя');
+	// 	} else this.currentIndex$.next(newIndex);
+	// 	//console.log(maxindex);
+	// }
+
 	private next() {
-		const nextIndex = this.currentIndex$.value + 1;
-		const maxindex =
-			Math.round(
-				(this.appPaginationOf as T[]).length / this.appPaginationChankSize,
-			) - 1;
-		const newIndex = nextIndex <= maxindex ? nextIndex : maxindex;
-		if (this.currentIndex$.value == maxindex) {
-			console.log('Последняя');
-		} else this.currentIndex$.next(newIndex);
-		//console.log(maxindex);
+		console.log(this.chanks);
+		const currentIndex = this.currentIndex$.value;
+		const maxIndex = this.chanks.length - 1;
+
+		if (currentIndex >= maxIndex) {
+			console.log('Находимся на последнем элементе');
+
+			return;
+		}
+
+		this.currentIndex$.next(currentIndex + 1);
 	}
 
 	private back() {
